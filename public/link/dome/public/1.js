10a3b533ff92ac007cbc8f367077133bfedfbb22
minJonRing <1014080407tqr@gmail.com>
2018-12-29 17:54:49 +0800

    1

---
+++ public/link/dome/public/1.js
@@ -0,0 +1,24 @@
+let video = document.querySelector(".video");
+let p = document.querySelector("p");
+let can = document.querySelector(".can");
+let ctx = can.getContext('2d');
+let w = video.clientWidth;
+let h = video.clientHeight;
+p.style.height = h +"px";
+can.width = video.clientWidth;
+can.height = video.clientHeight;
+video.oncanplay = function(){
+    p.style.display = "none";
+    video.play();
+    video.addEventListener('play', function(){
+    //画布上画视频，需要动态地获取它，一帧一帧地画出来
+        setInterval(function(){
+            handleDrew()
+            }, 20);
+    });
+}
+
+function handleDrew(){
+    ctx.clearRect(0,0,w,h);
+    ctx.drawImage(video, 0, 0, w, h);
+}
